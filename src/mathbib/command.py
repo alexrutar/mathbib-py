286d18e51ae0afdb2b05e7746d9ec96a1673ff85
Alex Rutar <alex@rutar.org>
2023-05-11 12:05:31 +0200

    fix formatting

--- src/mathbib/command.py
+++ src/mathbib/command.py
@@ -3,22 +3,27 @@ from xdg_base_dirs import xdg_data_home
 
 from .index import list_records, generate_records_from_storage
 
+
 def cli():
     if len(sys.argv) == 1:
         print("Must specify command!")
+
     else:
         match sys.argv[1]:
-            case 'file':
+            case "file":
                 key, identifier = sys.argv[2].split(":")
-                candidate = xdg_data_home() / "mathbib" / "files" / key / f"{identifier}.pdf"
+                candidate = (
+                    xdg_data_home() / "mathbib" / "files" / key / f"{identifier}.pdf"
+                )
                 if candidate.exists():
                     print(candidate)
 
-            case 'list':
+            case "list":
                 for record in list_records():
                     print(record)
 
-            case 'refresh':
+            case "refresh":
                 generate_records_from_storage()
+
             case _:
                 print(f"Invalid command '{sys.argv[1]}'")
